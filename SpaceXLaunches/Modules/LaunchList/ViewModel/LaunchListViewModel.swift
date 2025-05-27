379d581bbe67fb26836872ad564c6a23dc1de300
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2025-05-26 21:37:12 -0600

    Feature: LaunchListViewModel added func fetchLaunches
    using combine to get data using the service and store in a set of cancellables

--- SpaceXLaunches/Modules/LaunchList/ViewModel/LaunchListViewModel.swift
+++ SpaceXLaunches/Modules/LaunchList/ViewModel/LaunchListViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import Combine
+
+final class LaunchListViewModel {
+
+    @Published private(set) var launches: [LaunchModel] = []
+    @Published private(set) var isLoading = false
+    @Published private(set) var errorMessage: String?
+
+    private let service: LaunchesServiceProtocol
+    private var cancellables = Set<AnyCancellable>()
+
+    init(service: LaunchesServiceProtocol) {
+        self.service = service
+    }
+
+    func fetchLaunches() {
+        isLoading = true
+        errorMessage = nil
+
+        service.fetchLaunches()
+            .sink { [weak self] completion in
+                self?.isLoading = false
+                if case .failure(let error) = completion {
+                    self?.errorMessage = error.localizedDescription
+                }
+            } receiveValue: { [weak self] launches in
+                self?.launches = launches
+            }
+            .store(in: &cancellables)
+    }
+}
