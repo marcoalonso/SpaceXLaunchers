abec7a3a9cb7fc3e78e997c37c461381ce850713
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2025-05-27 09:56:35 -0600

    Feature: Showing images on collectionView on launch details
    can change current image selecting index of page control and show video when taps RedButtonVideo

--- SpaceXLaunches/Modules/LaunchDetails/Views/LaunchDetailViewController.swift
+++ SpaceXLaunches/Modules/LaunchDetails/Views/LaunchDetailViewController.swift
@@ -12,7 +12,6 @@ import Kingfisher
 class LaunchDetailViewController: UIViewController {
     
     @IBOutlet weak var siteLabel: UILabel!
-    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var rocketNameLabel: UILabel!
     @IBOutlet weak var rocketTypeLabel: UILabel!
@@ -21,7 +20,12 @@ class LaunchDetailViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
     
+    @IBOutlet weak var collectionHeightConstraint: NSLayoutConstraint!
+    weak var coordinatorDelegate: LaunchDetailCoordinatorDelegate?
+    
     private let viewModel: LaunchDetailViewModel
+    
+    private var images: [String] = []
     
     init(viewModel: LaunchDetailViewModel) {
         self.viewModel = viewModel
@@ -34,20 +38,86 @@ class LaunchDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupCollectionView()
+        configurePageControl()
         setupUI()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.itemSize = CGSize(width: collectionView.bounds.width, height: 150)
+        }
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.isPagingEnabled = true
+        collectionView.register(UINib(nibName: "ImageCell", bundle: nil), forCellWithReuseIdentifier: "ImageCell")
+
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.itemSize = collectionView.frame.size
+        collectionView.collectionViewLayout = layout
+
+        images = viewModel.flickrImages
+        let shouldHideCarousel = images.isEmpty
+        collectionHeightConstraint.constant = shouldHideCarousel ? 0 : 190
+        collectionView.isHidden = shouldHideCarousel
+        pageControl.isHidden = shouldHideCarousel
+    }
+
+    private func configurePageControl() {
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.hidesForSinglePage = true
     }
     
     private func setupUI() {
         title = viewModel.missionName
-        titleLabel.text = viewModel.missionName
         siteLabel.text = "Site: \(viewModel.siteName)"
         dateLabel.text = "Date: \(viewModel.date)"
         rocketNameLabel.text = "Rocket name: \(viewModel.rocketName)"
         rocketTypeLabel.text = "Rocket type: \(viewModel.rocketType)"
         descriptionLabel.text = viewModel.details
-        
-        // setupImageCarousel()
+    }
+    
+    @IBAction func pageControlValueChanged(_ sender: UIPageControl) {
+        let page = sender.currentPage
+        let offset = CGPoint(x: collectionView.frame.width * CGFloat(page), y: 0)
+        collectionView.setContentOffset(offset, animated: true)
+    }
+    
+    @IBAction func openYouTubeFullScreen(_ sender: UIButton) {
+        guard let videoId = viewModel.youtubeId else { return }
+        coordinatorDelegate?.didTapYouTubeVideo(videoId: videoId)
+    }
+}
+
+extension LaunchDetailViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as? ImageCell else {
+            return UICollectionViewCell()
+        }
+
+        cell.configure(with: images[indexPath.item])
+        return cell
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = Int(scrollView.contentOffset.x / scrollView.frame.width)
+        pageControl.currentPage = page
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let page = Int(scrollView.contentOffset.x / scrollView.frame.width)
+        pageControl.currentPage = page
+    }
 }
